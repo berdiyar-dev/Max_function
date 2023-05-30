883b19bfafd1c0bbcded30bcace63631c50525cd
berdiyar-desktop <berdiyare@gmail.com>
2023-05-31 00:34:57 +0500

    user2

--- max.py
+++ max.py
@@ -1,5 +1,5 @@
 def param(n1,n2):
     return max(n1,n2)
 num1,num2 = map(int,input("Son kiriting: . . .").split())
-maxim = param(num1,num2)
-print(maxim)
+maxim-son = param(num1,num2)
+print(maxim-son)
